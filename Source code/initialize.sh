44de662a787b704230ca4dd2e87b5f7098cd4c2b
gregoiremassot <gregoire.massot@orange.fr>
2016-04-16 17:33:26 +0200

    Update initialize.sh

--- Source code/initialize.sh
+++ Source code/initialize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Initialize hadoop and load the input files
-
+JAVA_HOME=/usr/java/jdk1.7.0_67-cloudera/
 export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop fs -mkdir input
-hadoop fs -put tiny_graph.txt
+hadoop fs -put tiny_graph.txt input
